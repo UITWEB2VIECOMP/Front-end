247e6dabbd37ded38da0f7d9e483cb2bc2f71c8e
nhad1702 <anhducnguyenhuu2@gmail.com>
2024-07-20 16:20:04 +0700

    adjust sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Home.css';
 import Aside from '../components/Aside';
-// import Profile from './Profile';
 
 const Home = () => {
   const [role, setRole] = useState(null);
@@ -10,18 +9,15 @@ const Home = () => {
   useEffect(() => {
     const storedRole = localStorage.getItem('role');
     const storedId = localStorage.getItem('user_id');
-    // const storedName = localStorage.getItem('name')
     setRole(storedRole);
     setId(storedId);
   }, []);
 
   return (
-    <div className='home-page'>
-      <div className="aside-bar" style={{ display: "inline-block" }}>
-        {role && id ? <Aside id={id} role={role} /> : <div>Loading...</div>}
-      </div>
+    <div className="home-container">
+      {role && id ? <Aside id={id} role={role} /> : <div>Loading...</div>}
       <div className="home-content">
-        
+        <h1>Hello world</h1>
       </div>
     </div>
   );
